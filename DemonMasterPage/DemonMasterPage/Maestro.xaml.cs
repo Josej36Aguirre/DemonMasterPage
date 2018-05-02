b0d85bfb3dcab9bce3338096d6cd4ea90b15ff5f
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-05-02 01:20:37 -0500

    Initial commit

---
+++ DemonMasterPage/DemonMasterPage/Maestro.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemonMasterPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Maestro : MasterDetailPage
+	{
+		public Maestro ()
+		{
+			InitializeComponent ();
+           // btnMaestr2.Clicked += BtnMaestr2_Clicked;
+
+        }
+
+        //private void BtnMaestr2_Clicked(object sender, EventArgs e)
+        //{
+        //    ((NavigationPage)this.Parent).Navigation.PushAsync(new Detalle());
+        //}
+    }
+}
---
+++ DemonMasterPage/DemonMasterPage/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemonMasterPage
+{
+	public partial class MainPage : MasterDetailPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+           // btnMaestro.Clicked += BtnMaestro_Clicked;
+
+        }
+
+        //private void BtnMaestro_Clicked(object sender, EventArgs e)
+        //{
+        //    ((NavigationPage)this.Parent).Navigation.PushAsync(new Maestro());
+        //}
+    }
+}
